d122bfe5db6ff21116d6e324bda9fd7860200420
0x7b <0x7b123@gmail.com>
2025-09-03 14:14:52 +0900

    死亡者・留置者の記録をできるようにした

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const personState = {
     checkList: Array(10).fill(false),
-    takenCardList: Array(10).fill(false)
+    takenCardList: Array(10).fill(false),
+    deadList: Array(10).fill(0),
 }
 
 const dict = {
@@ -21,6 +22,11 @@ people.forEach((personName, index) => {
     const div = document.createElement('div');
     div.id = personName;
 
+    const deadFillter = document.createElement('div'); 
+    deadFillter.id = `${personName}_dead_fillter`;
+    deadFillter.className = 'person_dead_fillter';
+    deadFillter.style.display = 'none';
+
     const innerElement = document.createElement('div'); 
     innerElement.id = `${personName}_name`;
     innerElement.className = 'person_name';
@@ -36,21 +42,26 @@ people.forEach((personName, index) => {
     takenCard.className = 'person_takenCard_before  hover_big';
     takenCard.textContent = `身分証所持`;
 
-    // const checkBox = document.createElement('input');
-    // checkBox.type = 'checkbox';
-    // checkBox.label = 'キラ陣営';
-    // checkBox.name = 'キラ陣営';
+    const dead = document.createElement('div'); 
+    dead.id = `${personName}_deadState`;
+    dead.className = 'person_alive  hover_big';
+    dead.textContent = `生存`;
 
-    // const label = document.createElement('label');
-    // label.for = 'キラ陣営'
-    // label.textContent = 'キラ陣営か?'
+    const deadMark = document.createElement('img'); 
+    deadMark.id = `${personName}_dead_mark`;
+    deadMark.className = 'person_dead_mark';
+    deadMark.src = `../publicdomainq-0035421kjvdum.png`;
+    deadMark.style.display = 'none';
 
     
+
+    div.appendChild(deadFillter)
     div.appendChild(innerElement);
     div.appendChild(checked);
     div.appendChild(takenCard)
-    // div.appendChild(checkBox)
-    // div.appendChild(label)
+    div.appendChild(dead)
+    div.appendChild(deadMark)
+
 
     div.addEventListener('click', () => {
         check_person(index); 
@@ -60,6 +71,13 @@ people.forEach((personName, index) => {
         event.stopPropagation()
         changeTakenCard(index);    
     })
+
+    dead.addEventListener('click', (event) =>{
+        event.stopPropagation()
+        changeAlive(index);    
+    })
+
+    
     personContainer.appendChild(div);
 });
 
@@ -77,8 +95,34 @@ function changeTakenCard(index){
     const target = personState.takenCardList[index];
     personState.takenCardList[index] = (target === true) ? false : true;
     let element = document.getElementById(`${dict[index]}_takenCard`);
-    element.className = (target === true) ? 'person_takenCard_before' : 'person_takenCard_after';
-    element.textContent = (target === true) ? '身分証所持' : `身分証盗難`;
+    element.className =  (target === true) ? 'person_takenCard_before hover_big' : 'person_takenCard_after hover_big';
+    element.textContent = (target === true) ? '身分証所持' : '身分証盗難';
 }
 
+function changeAlive(index){
+    const target = personState.deadList[index];
+    personState.deadList[index] += 1;
+    let element = document.getElementById(`${dict[index]}_deadState`);
+    let deadFillter = document.getElementById(`${dict[index]}_dead_fillter`)
+    let deadMark = document.getElementById(`${dict[index]}_dead_mark`)
+
+    console.log(target)
+    if (target % 3 === 0){
+        element.className = 'person_dead hover_big';
+        element.textContent = '死亡';
+        deadFillter.style.display = '';
+        deadMark.style.display = '';
+        
+    } else if(target % 3 === 1){
+        element.className = 'person_detention hover_big';
+        element.textContent = '留置';
+        deadFillter.style.display = '';
+        deadMark.style.display = '';
+    } else {
+        element.className = 'person_alive hover_big';
+        element.textContent = '生存';
+        deadFillter.style.display = 'none';
+        deadMark.style.display = 'none';
+    }
+}
 
