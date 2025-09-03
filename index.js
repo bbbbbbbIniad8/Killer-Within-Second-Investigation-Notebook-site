c267a2c6a7e9e4036064879f4b6c1ddbe1d65dee
0x7b <0x7b123@gmail.com>
2025-09-03 12:51:01 +0900

    身分証の盗難を記録できるようにした

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const personState = {
-    checkList: Array(10).fill(false)
+    checkList: Array(10).fill(false),
+    takenCardList: Array(10).fill(false)
 }
 
 const dict = {
@@ -10,8 +11,11 @@ const people = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 const personContainer = document.querySelector('.person');
 const KillerSelect = document.getElementById('Killer-select')
 const subKillerSelect = document.getElementById('sub-Killer-select')
+const checkedText = "！"
 let whoKillerIndex = -1;
 let whosubKillerIndex = -1;
+
+
 
 people.forEach((personName, index) => { 
     const div = document.createElement('div');
@@ -25,7 +29,12 @@ people.forEach((personName, index) => {
     const checked = document.createElement('div'); 
     checked.id = `${personName}_checked`;
     checked.className = 'person_checked_before';
-    checked.textContent = 'Lの疑いアリ';
+    checked.textContent = '';
+
+    const takenCard = document.createElement('div'); 
+    takenCard.id = `${personName}_takenCard`;
+    takenCard.className = 'person_takenCard_before  hover_big';
+    takenCard.textContent = `身分証所持`;
 
     // const checkBox = document.createElement('input');
     // checkBox.type = 'checkbox';
@@ -39,53 +48,20 @@ people.forEach((personName, index) => {
     
     div.appendChild(innerElement);
     div.appendChild(checked);
+    div.appendChild(takenCard)
     // div.appendChild(checkBox)
     // div.appendChild(label)
 
     div.addEventListener('click', () => {
         check_person(index); 
     });
+
+    takenCard.addEventListener('click', (event) =>{
+        event.stopPropagation()
+        changeTakenCard(index);    
+    })
     personContainer.appendChild(div);
-
-    const option = document.createElement('option');
-    option.value = index;
-    option.textContent = personName;
-    KillerSelect.appendChild(option);
-
-    const option2 = document.createElement('option');
-    option2.value = index;
-    option2.textContent = personName;
-    subKillerSelect.appendChild(option2);
 });
-
-
-function selectProcessKiller(event){
-    const index = Number(event.target.value);
-    if (index != -1 && index != whosubKillerIndex){
-        if (personState.checkList[index] === false){
-            check_person(index)
-        }
-        
-        if (whoKillerIndex != -1){
-            check_person(whoKillerIndex)
-        }
-        whoKillerIndex = index;
-    }
-}
-
-function selectProcessSubKiller(event){
-    const index = Number(event.target.value);
-    if (index != -1 && index != whoKillerIndex){
-        check_person(index)
-        if (whosubKillerIndex != -1){
-            check_person(whosubKillerIndex)
-        }
-        whosubKillerIndex = index;
-    }
-}
-
-KillerSelect.addEventListener('change', selectProcessKiller);
-subKillerSelect.addEventListener('change', selectProcessSubKiller);
 
 
 function check_person(index){
@@ -93,22 +69,16 @@ function check_person(index){
     personState.checkList[index] = (target === true) ? false : true;
     let element = document.getElementById(`${dict[index]}_checked`);
     element.className = (target === true) ? 'person_checked_before' : 'person_checked_after' ;
-    element.textContent = (target === true) ? 'Lの疑いアリ' : 'L以外 確定';
+    element.textContent = (target === true) ? '' : `${checkedText}`;
 
 }
 
-function judge_who_L() {
-    const answer = personState.checkList
-        .map((isChecked, index) => isChecked === false ? dict[index] : null)
-        .filter(Boolean);
-    
-    if (answer.length <= 0) {
-        message = '夜神月: 何が何だかわからない...。';
-    } else if (answer.length <= 1){
-        message = `夜神月: Lはこの人物の可能性が非常に高い。\n[${answer.join(', ')}]\n 強硬手段を使うことをお勧めする。`;
-    } else {
-        message = `夜神月: Lはこの中に潜伏している可能性がある。\n[${answer.join(', ')}]`;
-    }
-
-    document.getElementById("judge-text").textContent = message;
+function changeTakenCard(index){
+    const target = personState.takenCardList[index];
+    personState.takenCardList[index] = (target === true) ? false : true;
+    let element = document.getElementById(`${dict[index]}_takenCard`);
+    element.className = (target === true) ? 'person_takenCard_before' : 'person_takenCard_after';
+    element.textContent = (target === true) ? '身分証所持' : `身分証盗難`;
 }
+
+
